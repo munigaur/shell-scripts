1f1fb7709d3a7a0e2870111976597db910d7fe5a
munigaur <munindragaur@gmail.com>
2023-01-03 14:25:52 +0530

    color typo rectify

--- colors.sh
+++ colors.sh
@@ -8,5 +8,5 @@ echo -e "\e[34m I am printing blue color \e[0m"
 echo -e "\e[35m I am printing cyan color \e[0m"
 
 #how to print background color
-echo -e "\e[41,33m welcome to color \e[0m"
-echo -e "\e[47,32m Avatar2 is very good film \e[0m"
+echo -e "\e[41;33m welcome to color \e[0m"
+echo -e "\e[47;32m Avatar2 is very good film \e[0m"
