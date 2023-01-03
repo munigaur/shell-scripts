001fbac4c412441dada36b332834792579e647ea
munigaur <munindragaur@gmail.com>
2023-01-03 14:23:34 +0530

    color script

---
+++ colors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#how to print foreground color
+echo -e "\e[32m I am printing green color \e[0m"
+echo -e "\e[31m I am printing red color \e[0m"
+echo -e "\e[33m I am printing yellow color \e[0m"
+echo -e "\e[34m I am printing blue color \e[0m"
+echo -e "\e[35m I am printing cyan color \e[0m"
+
+#how to print background color
+echo -e "\e[41,33m welcome to color \e[0m"
+echo -e "\e[47,32m Avatar2 is very good film \e[0m"
