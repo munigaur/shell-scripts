85fb2d6a2c7010315a230d59a26edaa2e5950a52
munigaur <munindragaur@gmail.com>
2023-01-05 09:40:45 +0530

    changed

--- function.sh
+++ function.sh
@@ -10,7 +10,8 @@ sample() {
 stat() {
     echo "number of opened sessions : $(who|wc -l)"
     echo "today's date is : $(date +%F)"
-    echo "load average on the system in last 1 minute is: $(uptime |awk -F : '{print $NF}' | awk -F , '{print $1}'"
+    echo "load average on the system in last 1 minute is: $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
+    
     echo -e "\e[32m ___ stat function complete___ \e[0m"
 
     #calling another function
