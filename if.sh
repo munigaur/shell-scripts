5c2c3e4b443a4db33a87777e84ef538d1e0064e5
munigaur <munindragaur@gmail.com>
2023-01-09 09:04:08 +0530

    new

--- if.sh
+++ if.sh
@@ -3,10 +3,10 @@
 a=$1
 
 if ["$a" == "xyz" ]; then
-echo -e "\e [32m both of them are equal \e[0m"
+echo -e "\e[32m both of them are equal \e[0m"
 
 else
-echo -e "\e [32m both of them are not equal \e[0m"
+echo -e "\e[32m both of them are not equal \e[0m"
 exit 1
 
 fi
