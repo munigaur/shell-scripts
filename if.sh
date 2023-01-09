e2a95db8f38dc63db0f5fd579576dc24ea537420
munigaur <munindragaur@gmail.com>
2023-01-09 09:20:14 +0530

    number if

--- if.sh
+++ if.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-a=$1
+c=$1
 
-if [ "$a" == "xyz" ]; then
-echo -e "\e[32m both of them are equal \e[0m"
+if [ "$c" -eq "10" ] ; then
+echo "value is 10"
+
+elif [ "$c" -eq "20" ] ; then
+echo "value is 20"
+
+elif [ "$c" -eq "30" ] ; then
+echo "value is 30"
 
 else
-echo -e "\e[32m both of them are not equal \e[0m"
-exit 1
+echo "value is not 10, 20 or 30"
+exit 100
 
 fi
+
