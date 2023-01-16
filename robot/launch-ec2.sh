c2bf2239f1149029e3f7141b56be50adf000b755
munigaur <munindragaur@gmail.com>
2023-01-16 13:36:24 +0530

    launch try2

--- robot/launch-ec2.sh
+++ robot/launch-ec2.sh
@@ -11,7 +11,7 @@ if [ -z "$COMPONENT" ] || [ -z "$ENV" ]; then
 fi 
 
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g')
-SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52-allow-all --region us-east-1 | jq .SecurityGroups[].GroupId | sed -e 's/"//g')
+SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b52_allow_all --region us-east-1 | jq .SecurityGroups[].GroupId | sed -e 's/"//g')
 
 echo -e "AMI ID Used to launch the instance is \e[32m $AMI_ID \e[0m "
 echo -e "Security Group ID Used to launch the instance is \e[32m  $SG_ID \e[0m"
